6c94f70740738122e3ff2be73cc7dc832c017584
sane cheikh oumar <safietou2717@gmail.com>
2022-09-12 16:31:16 +0200

    fs

---
+++ path.js
@@ -0,0 +1,18 @@
+
+const path = require('path');
+console.log
+//affichage du nom du repertoire
+
+console.log(path.dirname('/'));
+console.monRep = path.dirname(
+    "c:\\users\\Apprenants\\Documents\\node.js\\path.js") ;
+    console.log(monRep);
+
+ //afficher le nom fichier
+ console.log(pathbasename());
+
+ 
+
+ //affichage de l'extension du fichier
+ path.basename(notes, path.extname(notes));
+
